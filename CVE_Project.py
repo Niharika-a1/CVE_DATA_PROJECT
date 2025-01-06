e504593fdb4123e6edc62360cf2b63d853d1ac8e
Niharika A <niharika.ambojipet@gmail.com>
2025-01-05 21:51:38 -0600

    CVE_Project.py

--- CVE_Project.py
+++ CVE_Project.py
@@ -12,7 +12,7 @@ def validate_cve_id(cve_id):
 
 # Utility function for validating severity
 def validate_severity(severity):
-    valid_severities = ['LOW', 'MEDIUM', 'HIGH', 'CRITICAL']
+    valid_severities = ['LOW', 'MEDIUM', 'HIGH']
     if severity.upper() not in valid_severities:
         raise ValueError(f"Invalid severity level. Choose from: {', '.join(valid_severities)}.")
 
@@ -25,7 +25,12 @@ def validate_date(date_string):
 
 # Database connection
 def connect_to_database():
-    connection = mysql.connector.connect(host='localhost',user='root',password='19121997' ,database='cve_database')
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='root',        
+        password='', #give your database password 
+        database='cve_database'
+    )
     return connection
 
 # Table creation
@@ -47,7 +52,7 @@ def create_table(connection):
 # Fetch CVE data from NVD API
 def fetch_cve_data():
     base_url = 'https://services.nvd.nist.gov/rest/json/cves/2.0'
-    total_to_fetch = 5  # Number of CVE entries you want to fetch
+    total_to_fetch = 1  # Number of CVE entries you want to fetch
 
     params = {
         'resultsPerPage': total_to_fetch,
@@ -55,8 +60,8 @@ def fetch_cve_data():
     }
 
     headers = {
-        'apiKey': '',  # generated_api_key      
-        'User-Agent': '' # username
+        'apiKey': '',  # generated_api_key        
+        'User-Agent': ''  # registered_email_id
     }
 
     response = requests.get(base_url, headers=headers, params=params)
@@ -240,7 +245,7 @@ def main():
         print(row)
 
     print("\nSearch by Severity:")
-    severity = input("Enter severity level (LOW, MEDIUM, HIGH, CRITICAL): ")
+    severity = input("Enter severity level (LOW, MEDIUM, HIGH): ")
     results = search_by_severity(connection, severity)
     print(f"Found {len(results)} entries with severity {severity.upper()}.")
     for row in results:
